5a22553d598313774e77e3f3fdec23fc7f9a1a0d
Abhishek Agrawal <abhishekagrawal7340@gmail.com>
2026-09-01 21:34:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://student-mental-health-predictor-390e.onrender.com/";
 const API_URL = `${API_BASE_URL}/predict`;
 
 const form = document.getElementById("predict-form");
